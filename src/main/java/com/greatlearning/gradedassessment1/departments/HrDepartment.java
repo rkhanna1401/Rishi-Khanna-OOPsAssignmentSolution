3564dae9d014f3f425cc05a0a03ef0c2e90c9d4b
rkhanna1401 <86465364+rkhanna1401@users.noreply.github.com>
2021-11-29 11:29:55 +0530

    Update HrDepartment.java

--- src/main/java/com/greatlearning/gradedassessment1/departments/HrDepartment.java
+++ src/main/java/com/greatlearning/gradedassessment1/departments/HrDepartment.java
@@ -5,13 +5,17 @@ public class HrDepartment extends SuperDepartment{
 	public String departmentName() {
 		return " Hr Department ";
 	}
+	
 	public String getTodaysWork() {
 		return "Fill today’s timesheet and mark your attendance";
 	}
+	
 	public String getWorkDeadline() {
 		return "Complete by EOD ";
 	}
+	
 	public String doActivity() {
 		return "team Lunch";
 	}
+	
 }
